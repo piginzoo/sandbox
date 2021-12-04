1a767b6bf305d76e157d6d7bb88ed9b3b48ed749
piginzoo <piginzoo@gmail.com>
2021-12-04 13:56:08 +0800

    update

--- pytorch/simple2.py
+++ pytorch/simple2.py
@@ -10,27 +10,18 @@ from torch.autograd import Variable
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)  # 1 input image channel, 6 output channels, 5x5 square convolution kernel
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # an affine operation: y = Wx + b
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(32*32, 512)  # an affine operation: y = Wx + b
+        self.bn1 = nn.BatchNorm1d(512),
+        self.fc2 = nn.Linear(512, 2)
+        self.bn2 = nn.BatchNorm1d(2)
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))  # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)  # If the size is a square you can only specify a single number
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
         return x
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
 
 
 net = Net()
@@ -42,7 +33,7 @@ for p in params:
     print(p.shape)
 
 print("输入：")
-input = Variable(torch.randn(1, 1, 32, 32))
+input = Variable(torch.randn(1, 32*32))
 print(input)
 
 print("前向")
@@ -52,7 +43,7 @@ print("网络梯度全部初始化为0")
 net.zero_grad()  # 对所有的参数的梯度缓冲区进行归零
 
 criterion = nn.MSELoss()
-y_label = Variable(torch.range(1, 10))  # <--- 输出y=y_label
+y_label = Variable(torch.range(1, 2))  # <--- 输出y=y_label
 loss = criterion(out, y_label)
 
 print('conv1.bias.grad before backward')
@@ -70,7 +61,7 @@ for epoch in range(2):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i in range(3):
-        input = Variable(torch.randn(1, 1, 32, 32))
+        input = Variable(torch.randn(1, 32 * 32))
         output = net(input)
         loss = criterion(output, y_label)
         loss.backward()
@@ -82,7 +73,6 @@ for epoch in range(2):  # loop over the dataset multiple times
         print('[%d, %5d] loss值: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
         running_loss = 0.0
 print('训练结束.....')
-
 
 print("尝试预测：")
 outputs = net(Variable(torch.randn(1, 1, 32, 32)))
